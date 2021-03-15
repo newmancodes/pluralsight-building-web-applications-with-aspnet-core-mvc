4f8ca924b204f3f235dad141b3ab26cd89a3f113
Stephen Newman <steve@newman.digital>
2021-03-15 17:13:05 +0000

    Module 4 - Creating the List Page

---
+++ BethanysPieShop/Controllers/PieController.cs
@@ -0,0 +1,33 @@
+﻿using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository pieRepository;
+        private readonly ICategoryRepository categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            this.pieRepository = pieRepository;
+            this.categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            var piesListViewModel = new PiesListViewModel
+            {
+                CurrentCategory = "Cheesecakes",
+                Pies = this.pieRepository.AllPies,
+            };
+
+            return View(piesListViewModel);
+        }
+    }
+}
