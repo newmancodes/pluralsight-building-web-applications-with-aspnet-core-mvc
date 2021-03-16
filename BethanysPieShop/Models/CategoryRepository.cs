9166a305bc619da802e955cdd707c512a6e8bad9
Stephen Newman <steve@newman.digital>
2021-03-16 08:42:17 +0000

    Module 5 - Working with RealData Using Entity Framework Core

---
+++ BethanysPieShop/Models/CategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly AppDbContext appDbContext;
+
+        public IEnumerable<Category> AllCategories => this.appDbContext.Categories;
+
+        public CategoryRepository(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+    }
+}
---
+++ BethanysPieShop/Models/PieRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class PieRepository : IPieRepository
+    {
+        private readonly AppDbContext appDbContext;
+
+        public IEnumerable<Pie> AllPies => this.appDbContext.Pies.Include(p => p.Category);
+
+        public IEnumerable<Pie> PiesOfTheWeek => this.appDbContext.Pies.Include(p => p.Category).Where(p => p.IsPieOfTheWeek);
+
+        public PieRepository(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        public Pie GetPieByPieId(int pieId)
+        {
+            return this.appDbContext.Pies.Include(p => p.Category).SingleOrDefault(p => p.PieId == pieId);
+        }
+    }
+}
